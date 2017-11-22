d18d244ede0819c82742cd22163cbfa83b8ce87d
z1234567cf <294300483@qq.com>
2017-11-22 17:47:19 +0800

    almost

--- newPingbanche/js/common.js
+++ newPingbanche/js/common.js
@@ -41,6 +41,43 @@ function leaveHideen() {
 function readyHandle() {
     $("#js_introduce_login").mouseenter(mouseEnterHandle);
     $("#js_introduce_login").mouseleave(mouseLeaveHandle);
-    $("#js_head_wx").click(clickHandle);
+    $("#js_head_wx").mouseenter(clickHandle);
     $("#js_head_wx").mouseleave(leaveHideen);
+    showObject.clickTitle();
 }
+//板块点击显示消失
+var showObject={
+    slecttTitle:function (title) {
+        return $(title);
+    },
+    selectCotain:function (contain) {
+        return $(contain)
+    },
+    clickTitle:function () {
+        var select=showObject.slecttTitle(".js_select_title a");
+        console.log(select);
+        select.each(function (i) {
+            var _this=this;
+            var contain=showObject.selectCotain(".js_select_contain");
+            var bg=showObject.selectCotain(".js_select_bg");
+            $(this).click(function () {
+                console.log(_this);
+                showObject.changClass(select,_this,"select-btns-click1");
+                showObject.changClass(contain,_this,"show");
+                showObject.changClass(bg,_this,"show");
+            })
+        })
+    },
+    changClass:function (obj,_this,_class) {
+        for(var i=0;i<obj.length;i++){
+            if($(_this).attr("data-match")==$(obj[i]).attr("data-match")){
+                $(obj[i]).removeClass(_class);
+                $(obj[i]).addClass(_class);
+            }else {
+                $(obj[i]).removeClass(_class);
+            }
+        }
+    },
+
+};
+
